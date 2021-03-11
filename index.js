32194184f5199f0c45ae779aa9350adde0f7b5a2
LucasDM1 <lucas19651@hotmail.com>
2021-03-11 21:50:39 +0000

    (Feature): Form Validation

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ let State = document.getElementById("inputState");
 let PostalCode = document.getElementById("inputZip");
 let Messages = document.getElementById("Messages");
 
-myForm.addEventListener('submit', e => {//evitar el submit
-    e.preventDefault();
-    console.log(FirstName.id);
+myForm.addEventListener('submit', e => {
+    e.preventDefault();//evitar el submit
+    // console.log(FirstName.id);
     checkInputs();//comprobar los inputs
-   
+
 });
 
-function checkInputs(){
+function checkInputs() {
     //Numbers
-    const CardNumberValue =  CardNumber.value;
+    const CardNumberValue = CardNumber.value;
     const CardCVCValue = CardCVC.value;//000
     const AmountValue = Amount.value;
     const PostalCodeValue = PostalCode.value;
@@ -29,81 +29,109 @@ function checkInputs(){
     const CityValue = City.value;
     const StateValue = State.value;
     const MessagesValue = Messages.value;
-    
-    if(CardNumberValue==''||CardCVCValue==''||AmountValue==''||PostalCodeValue==''||FirstNameValue==''||LastNameValue==''||CityValue==''||StateValue=='Pick a State'||MessagesValue==''){
+
+    if (CardNumberValue == '' || CardCVCValue == '' || AmountValue == '' || PostalCodeValue == '' || FirstNameValue == '' || LastNameValue == '' || CityValue == '' || StateValue == 'Pick a State' || MessagesValue == '') {
         empty();
-        
-    }else{
+
+    } else {
         Full();
     };
+
     //Numbers validation
-    if(CardNumberValue.length != 17||CardNumberValue=='') {
-		InputError(CardNumber, 'Card Number should have 17 digits');
-	} else {
-		InputSuccess(CardNumber);
+    if (CardNumberValue.length != 17 || CardNumberValue == '') {
+        InputError(CardNumber, 'Card Number should have 17 digits');
+    } else {
+        InputSuccess(CardNumber);
     };
 
-    if(CardCVCValue.length != 3||CardCVCValue=='') {
-		InputError(CardCVC, 'Card CVC should have 3 digits');
-	} else {
-		InputSuccess(CardCVC);
+    if (CardCVCValue.length != 3 || CardCVCValue == '') {
+        InputError(CardCVC, 'Card CVC should have 3 digits');
+    } else {
+        InputSuccess(CardCVC);
     };
 
-    if(AmountValue.length == 0 ||AmountValue=='') {
-		InputError(Amount, 'Amount should be more than 0');
-	} else {
-		InputSuccess(Amount);
+    if (AmountValue.length == 0 || AmountValue == '') {
+        InputError(Amount, 'Amount should be more than 0');
+    } else {
+        InputSuccess(Amount);
     };
-    
-    if(PostalCodeValue.length != 5||PostalCodeValue=='') {
-		InputError(PostalCode, 'Insert a valid postal code');
-	} else {
-		InputSuccess(PostalCode);
+
+    if (PostalCodeValue.length != 5 || PostalCodeValue == '') {
+        InputError(PostalCode, 'Postal Code should have 5 digits');
+    } else {
+        InputSuccess(PostalCode);
     };
+
     //strings validation
-    if(typeof FirstNameValue == "number" || CardNumberValue == '') {
-		InputError(FirstName, 'Insert a valid name');
-	} else {
-		InputSuccess(FirstName);
+    if (FirstNameValue == '') {
+        InputError(FirstName, 'Insert a valid name');
+    } else {
+        InputSuccess(FirstName);
     };
-   
-    
-	
-    
-    
-    
-        
-    
+
+    if (LastNameValue == '') {
+        InputError(LastName, 'Insert a valid lastname');
+    } else {
+        InputSuccess(LastName);
+    };
+
+    if (CityValue == '') {
+        InputError(City, 'Insert a valid City');
+    } else {
+        InputSuccess(City);
+    };
+
+    if (StateValue == 'Pick a State') {
+        InputError(State, 'Insert a valid State');
+    } else {
+        InputSuccess(State);
+    };
+
+    if (MessagesValue == '') {
+        InputError(Messages, 'Please write a message');
+    } else {
+        InputSuccess(Messages);
+    };
+
+
+
+
+
+
 };
+
 //input lo voy a ocupar para el fondo alert
-function empty(){
-    document.getElementById("Warning").innerHTML=`<div class="col-12">
+function empty() {
+    document.getElementById("Warning").innerHTML = `<div class="col-12">
                                     <div class="alert alert-danger" role="alert">
                                         <strong>Some fields are missing</strong>
                                     </div>
                                      </div>`;
 };
 
-function Full(){
-    document.getElementById("Warning").innerHTML=" "
+function Full() {
+    document.getElementById("Warning").innerHTML = " "
 };
 
 
-function InputError(input, msg){
-    
-    input.style.background="#f8d7da";
+function InputError(input, msg) {
+
+    input.style.background = "#f8d7da";
     const div = input.parentElement;
-    let littlemsg=document.createElement('small');
-    littlemsg.innerHTML=`${msg}`;
-    div.appendChild(littlemsg);}
-    
-    
-};
-function InputSuccess(input){
-    input.style.background="white"
-    const div = input.parentElement;
-    let small=div.querySelector("small");
-    small.innerText="";
+    //para evitar que se dupliquen los mensajes
+    let small = div.querySelector("small");
+    if (small.innerHTML =! '') { small.innerHTML = '' };
+    small.innerHTML=`${msg}`
+
 
 };
+function InputSuccess(input) {
+    input.style.background = "white"
+    const div = input.parentElement;
+    let small = div.querySelector("small");
+    small.innerHTML = '' 
+
+};
+
+
 
